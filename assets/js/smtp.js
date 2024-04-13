0e378293c6e4ac1f60f6ba915360f8132f383f06
NishantP43 <nishant.patil0403@gmail.com>
2024-04-13 16:29:18 +0530

    modified:   assets/js/smtp.js 	modified:   index.html

--- assets/js/smtp.js
+++ assets/js/smtp.js
@@ -1,10 +1,11 @@
+const form = document.querySelector("form");
 function sendEmail() {
-    Email.send({
+        Email.send({
         Host: "smtp.gmail.com",
-        Username: "nishant.patil0403@gmail.com",
-        Password: "Kim@ya2912",
+        Username: "nishantp920@gmail.com",
+        Password: "4ED7A5A9BB2FE84FE8C616395B4A9EDF59BE2",
         To: 'nishantp920@gmail.com',
-        From: document.getElementById("mail").value,
+        From: 'nishantp920@gmail.com',
         Subject: "This is Something",
         Body: "And this is the body"
     }).then(
@@ -15,3 +16,7 @@ function sendEmail() {
 function resetForm() {
     document.querySelector(".contact__form").reset();
 }
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    sendEmail();
+})
